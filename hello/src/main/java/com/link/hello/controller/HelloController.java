6ba75d399e5e1476d70ed5ffc410418dc6589ca9
ecaterina99 <tricolicikatea1999@gmail.com>
2025-04-22 22:30:49 +0300

    Initial commit

---
+++ hello/src/main/java/com/link/hello/controller/HelloController.java
@@ -0,0 +1,58 @@
+package com.link.hello.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloController {
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> hello() {
+        HttpHeaders headers = new HttpHeaders();
+        List<String> cookies = new ArrayList<>();
+        cookies.add("nr_inmatriculare=B-123-ABC");
+        cookies.add("sofer=Vasilica; Path=/");
+        headers.put("Set-Cookie", cookies);
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<>("Hello!", headers, HttpStatus.CREATED);
+
+
+        return responseEntity;
+    }
+
+    @GetMapping("/bye/{persoana}/{year}")
+    public String goodbye(
+            @PathVariable String persoana,
+            @PathVariable int year,
+            @RequestParam(defaultValue = "") String city, // request param == query param
+            @RequestParam(defaultValue = "-1") int km,
+            @RequestHeader("Cookie") String cookie
+    ) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Goodbye, ")
+                .append(persoana)
+                .append("! Anul este ")
+                .append(year)
+                .append(" Esti din orasul ")
+                .append(city);
+        if (km != -1) {
+            sb.append(" care se afla la ")
+                    .append(km)
+                    .append(" km");
+        }
+        sb.append(" Cookie-ul este: ").append(cookie);
+
+        return sb.toString();
+    }
+
+}
+
---
+++ hello/src/main/java/com/link/hello/controller/PrivateController.java
@@ -0,0 +1,17 @@
+package com.link.hello.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+public class PrivateController {
+    @RequestMapping(method = RequestMethod.GET, path="/dashboard")
+    public String dashboard(
+@RequestHeader(name = "Cookie", defaultValue = "")String cookie
+    ) {
+        if(cookie.isEmpty()){
+            return "Nu ai acces!";
+        }
+        return "Continut disponibil doar utilizatorilor autentificati!";
+    }
+}
