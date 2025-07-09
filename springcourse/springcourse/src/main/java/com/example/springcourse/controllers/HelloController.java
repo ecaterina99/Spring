e9021dc974735ec98590f770fe37954c65e8a062
ecaterina99 <tricolicikatea1999@gmail.com>
2025-07-09 10:37:39 +0300

    Spring Boot

---
+++ springcourse/springcourse/src/main/java/com/example/springcourse/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/hello")
+    public String getHello() {
+        System.out.println(this.hello);
+        return "hello";
+    }
+}
