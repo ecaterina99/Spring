a3d798497285525381540293aa2f22b2f1296537
ecaterina99 <tricolicikatea1999@gmail.com>
2025-07-10 20:01:28 +0300

    Cars Filter Web application using spring boot

---
+++ CarsFilterWebApp/src/main/java/com/link/CarsFilterWebApp/CarsFilterWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.link.CarsFilterWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsFilterWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsFilterWebAppApplication.class, args);
+	}
+
+}
---
+++ CarsFilterWebApp/src/test/java/com/link/CarsFilterWebApp/CarsFilterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.CarsFilterWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsFilterWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
