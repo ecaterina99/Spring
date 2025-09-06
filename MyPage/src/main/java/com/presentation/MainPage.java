bfec57485eb9d3e8fe08cf116c04d03a3444ef6c
ecaterina99 <tricolicikatea1999@gmail.com>
2025-09-06 12:06:42 +0300

    my presentation page

---
+++ MyPage/src/main/java/com/presentation/MainPage.java
@@ -0,0 +1,11 @@
+package com.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPage {
+    public static void main(String[] args) {
+        SpringApplication.run(MainPage.class, args);
+    }
+}
