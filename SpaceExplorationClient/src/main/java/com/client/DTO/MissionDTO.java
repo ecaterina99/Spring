29a3c92bfa5a7cc15a8c0b0cdba67438b89d02fd
ecaterina99 <tricolicikatea1999@gmail.com>
2025-10-02 11:25:58 +0300

    improve mission-card stylization

--- SpaceExplorationClient/src/main/java/com/client/DTO/MissionDTO.java
+++ SpaceExplorationClient/src/main/java/com/client/DTO/MissionDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Data
@@ -27,6 +28,15 @@ public class MissionDTO {
     private Integer paymentAmount;
     private MissionEnums.DifficultyLevel difficultyLevel;
     private List<MissionSpecializationsDTO> specializations = new ArrayList<>();
+
+    public String getShortDescription() {
+        if (description == null) return "";
+        String[] words = description.split("\\s+");
+        if (words.length <= 30) {
+            return description;
+        }
+        return String.join(" ", Arrays.copyOfRange(words, 0, 30)) + "...";
+    }
 
 }
 
